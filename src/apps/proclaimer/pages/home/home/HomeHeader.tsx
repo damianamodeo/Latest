7eb74d3509f403e201a2abf65683885e2edd756b
damianamodeo <damianamodeotheocratic@gmail.com>
2023-03-31 19:32:34 +1100

    remove docs folder

--- src/apps/proclaimer/pages/home/home/HomeHeader.tsx
+++ src/apps/proclaimer/pages/home/home/HomeHeader.tsx
@@ -5,7 +5,7 @@ type HomeHeaderType = {
 const HomeHeader = ({ changeSubpage }: HomeHeaderType) => {
   return (
     <div className={``} onClick={() => changeSubpage("Record", ">")}>
-      HomeHeader 0.4
+      HomeHeader 0.5
     </div>
   );
 };
