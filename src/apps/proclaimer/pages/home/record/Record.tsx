d6708e36680bd76625b563765cd2542f097c6065
damianamodeo <damianamodeotheocratic@gmail.com>
2023-04-28 22:47:06 +1000

    add address list on tabbed page

--- src/apps/proclaimer/pages/home/record/Record.tsx
+++ src/apps/proclaimer/pages/home/record/Record.tsx
@@ -1,15 +1,56 @@
 import TabbedPage from "@CONTAINERS/TabbedPage";
 import AddAddress from "./tabs/addAddress/AddAddress";
 import AddressList from "./tabs/addressList/AddressList";
+import { useEffect, useState } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { fdb } from "@SERVICES/firebase/config";
 
 const Record = () => {
+  const [mapDetails, setMapDetails] = useState<
+    {
+      id: string;
+      suburbs: { name: string; bbox: number[]; streets: [] }[];
+    }[]
+  >([]);
+
+  const [notAtHomesList, setNotAtHomesList] = useState(() => {
+    return;
+  });
+
+  useEffect(() => {
+    const unsub = onSnapshot(
+      doc(fdb, "notAtHomes", "MaitlandCongregation"),
+      (doc) => {
+        setNotAtHomesList(() => doc.data());
+      }
+    );
+    return unsub;
+  }, []);
+
+  useEffect(() => {
+    const unsub = onSnapshot(
+      doc(fdb, "australia_nsw_maitland", "maps"),
+      (doc) => {
+        if (doc.data()) {
+          setMapDetails(doc.data()?.mapDetails);
+        }
+      }
+    );
+    return () => {
+      unsub();
+    };
+  }, []);
+
   return (
     <div className={`h-full`}>
       <TabbedPage
-      color={"blue"}
+        color={"blue"}
         tabItems={[
-          { title: "Add", content: <AddAddress /> },
-          { title: "List", content: <AddressList /> },
+          { title: "Add", content: <AddAddress mapDetails={mapDetails} /> },
+          {
+            title: "List",
+            content: <AddressList notAtHomesList={notAtHomesList} />,
+          },
         ]}
       ></TabbedPage>
     </div>
--- src/apps/proclaimer/pages/home/record/tabs/addressList/AddressList.tsx
+++ src/apps/proclaimer/pages/home/record/tabs/addressList/AddressList.tsx
@@ -1,15 +1,50 @@
-type AddressListType = { 
-  children?: any
-  }
+type AddressListType = {
+  notAtHomesList?: any;
+};
 
-const AddressList = ({ children }: AddressListType) => {
+const AddressList = ({ notAtHomesList: addresses }: AddressListType) => {
   return (
-    <div className={``}>
-      AddressList
-      { children }
-      
+    <div className={`pb-16`}>
+      {addresses &&
+        Object.entries(addresses)
+          .filter((address: any) => {
+            return (
+              address[1].user ===
+              `${localStorage.getItem("initID")}_${localStorage.getItem(
+                "username"
+              )}`
+            );
+          })
+          .reverse()
+          .map((address: any, key: number) => {
+            return (
+              <div
+                key={key}
+                // onClick={() => handleClick(address)}
+                className="grid grid-cols-12 h-12 border-b dark:border-darkGrey-700 text-sm text-center"
+              >
+                <div className="col-span-2 px-2 my-auto font-bold">
+                  {address[1].mapNumber}
+                </div>
+                <div className="col-span-1 px-2 my-auto">
+                  {`${address[1].unitNumber && `${address[1].unitNumber}/`}${
+                    address[1].houseNumber
+                  }`}
+                </div>
+                <div className="col-span-4 my-auto">
+                  {`${address[1].street}`}
+                </div>
+                <div className="col-span-4 my-auto">{address[1].suburb}</div>
+                <div className="col-span-1 px-2 text-right my-auto">
+                  {/* <LongPress action={() => handleLongPress(address)}>
+                    {`${address[1].letter ? "L X" : "X"}`}
+                  </LongPress> */}
+                </div>
+              </div>
+            );
+          })}
     </div>
-  )
-}
+  );
+};
 
-export default AddressList
+export default AddressList;
