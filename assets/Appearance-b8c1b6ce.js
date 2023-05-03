148bf4166a82cc5da5a41bc195f1f28b8b603f74
damianamodeo <damianamodeotheocratic@gmail.com>
2023-05-03 21:39:09 +1000

    gh-pages deploy

--- assets/Appearance-b8c1b6ce.js
+++
@@ -1,1 +0,0 @@
-import{A as a}from"./index-1aca339a.js";const s=({changeSubpage:e})=>a.jsx("div",{className:"",onClick:()=>e("Settings","<"),children:"Appearance"});export{s as default};
---
+++ assets/AppearanceHeader-2fad7db3.js
@@ -0,0 +1,1 @@
+import{A as a}from"./index-7ec637ea.js";const s=({changeSubpage:e})=>a.jsx("div",{className:"",onClick:()=>e("Settings","<"),children:"AppearanceHeader"});export{s as default};
